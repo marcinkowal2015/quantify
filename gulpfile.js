6e98193c19d16c0f2d249d163b8916bc98b46468
marcinkowal2015 <marcinkowal2015@outlook.com>
2016-08-03 19:19:30 +0200

    Added support for sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
   console.log('default task')
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
