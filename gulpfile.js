16593e50a2fcfc657eb55043e109fd0ebdc5001a
marcinkowal2015 <marcinkowal2015@outlook.com>
2016-08-04 19:48:51 +0200

    Added merging scss files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('default', function() {
-  console.log('default task')
-});
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    concat = require('gulp-concat');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(cleanCSS())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('.'));
 });
+
+gulp.task('default', ['sass']);
