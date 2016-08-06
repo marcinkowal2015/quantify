43c9aac36df7a57837d38a1d4101c9e4572e72af
Dawid Krzyszycha <Dawid Krzyszycha>
2016-08-06 14:04:17 +0200

    footer changes

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
 <footer>
-
+    <div class="footer-width-wrap">
+        <div class="footer-menu">
+            tekst nagłówka
+            <ul>
+                <li>asd a</li>
+                <li>a a fd</li>
+                <li>asdf wrt</li>
+                <li> eyr 323</li>
+            </ul>
+        </div>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
