1af01e3d1e5f789e2a56232b3bfb9e8be2ba32c5
Dawid Krzyszycha <Dawid Krzyszycha>
2016-08-06 16:46:23 +0200

    Full footer styles + backend

--- footer.php
+++ footer.php
@@ -1,40 +1,41 @@
 <footer>
-    <div class="footer-width-wrap">
-        <div class="footer-menu">
-            <span class="menu-label">AGENCJA BADAWCZA</span>
-            <ul>
-                <li>asd a</li>
-                <li>a a fd</li>
-                <li>asdf wrt</li>
-                <li> eyr 323</li>
-            </ul>
+    <div class="footer-wrap">
+        <div class="footer-menu-wrap">
+            <?php
+
+            $args = array(
+                'theme_location' => 'footer_1'
+            );
+
+            wp_nav_menu($args) ?>
+
         </div>
-        <div class="footer-menu">
-            <span class="menu-label">AGENCJA BADAWCZA</span>
-            <ul>
-                <li>asd a</li>
-                <li>a a fd</li>
-                <li>asdf wrt</li>
-                <li> eyr 323</li>
-            </ul>
+        <div class="footer-menu-wrap">
+            <?php
+
+            $args = array(
+                'theme_location' => 'footer_2'
+            );
+
+            wp_nav_menu($args) ?>
+
         </div>
-        <div class="footer-menu">
-            <span class="menu-label">AGENCJA BADAWCZA</span>
-            <ul>
-                <li>asd a</li>
-                <li>a a fd</li>
-                <li>asdf wrt</li>
-                <li> eyr 323</li>
-            </ul>
+        <div class="footer-menu-wrap">
+            <?php
+
+            $args = array(
+                'theme_location' => 'footer_2'
+            );
+
+            wp_nav_menu($args) ?>
+
         </div>
-        <div class="footer-menu">
-            <span class="menu-label">AGENCJA BADAWCZA</span>
-            <ul>
-                <li>asd a</li>
-                <li>a a fd</li>
-                <li>asdf wrt</li>
-                <li> eyr 323</li>
-            </ul>
+        <div class="footer-menu-wrap">
+            <?php if ( is_active_sidebar( 'footer_column_4' ) ) : ?>
+                <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+                    <?php dynamic_sidebar( 'footer_column_4' ); ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </footer>
--- functions.php
+++ functions.php
@@ -1,12 +1,23 @@
 <?php function add_supports(){
+
+    //Add thumbnails support
     add_theme_support('post-thumbnails');
-//    register_nav_menus();
+
+    //Add navigation menus
+    register_nav_menus(array(
+        'footer_1' => __('Footer Menu 1'),
+        'footer_2' => __('Footer Menu 2'),
+        'footer_3' => __('Footer Menu 3'),
+        'footer_4' => __('')
+    ));
+
 }
 add_action('after_setup_theme' , 'add_supports');
 
 function resources(){
     wp_enqueue_style( 'style', get_template_directory_uri().'/style.css' );
 }
+add_action( 'wp_enqueue_scripts', 'resources' );
 
 function register_jquery() {
 	if (!is_admin() && $GLOBALS['pagenow'] != 'wp-login.php') {
@@ -15,6 +26,22 @@ function register_jquery() {
 		wp_enqueue_script('jquery');
 	}
 }
-
 add_action( 'init', 'register_jquery' );
-add_action( 'wp_enqueue_scripts', 'resources' );
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+    register_sidebar( array(
+        'name'          => 'Footer dane kontaktowe',
+        'id'            => 'footer_column_4',
+        'before_widget' => '<div class="contact-form">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="title">',
+        'after_title'   => '</h2>',
+    ) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
