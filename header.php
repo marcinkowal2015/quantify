921b65dd9ce520306100229a821e0a45bd230630
marcinkowal2015 <marcinkowal2015@outlook.com>
2016-08-06 14:03:46 +0200

    First version of header

--- header.php
+++ header.php
@@ -9,6 +9,20 @@
 <body <?php body_class(); ?>>
 
 <header>
-
+    <div class="header__bar">
+        <img src="<?php echo get_template_directory_uri()?>/img/logo.png">
+        <div class="header__navigation">
+            <div>
+                <span>aaa</span>
+                <span>aaa</span>
+                <span>aaa</span>
+                <span>aaa</span>
+            </div>
+            <div class="header__language">
+                <span class="header__language__pl">pl</span>
+                <span>en</span>
+            </div>
+        </div>
+    </div>
 
 </header>
