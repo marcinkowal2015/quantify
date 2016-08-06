34233e72bbc008bb96ea0f3c3f7b99428c236eee
marcinkowal2015 <marcinkowal2015@outlook.com>
2016-08-06 16:41:18 +0200

    Replaced wordpress jQuery with lastest version from CDN

--- functions.php
+++ functions.php
@@ -7,4 +7,14 @@ add_action('after_setup_theme' , 'add_supports');
 function resources(){
     wp_enqueue_style( 'style', get_template_directory_uri().'/style.css' );
 }
+
+function register_jquery() {
+	if (!is_admin() && $GLOBALS['pagenow'] != 'wp-login.php') {
+		wp_deregister_script('jquery');
+		wp_register_script('jquery','https://code.jquery.com/jquery-3.1.0.min.js', false, '3.1.0');
+		wp_enqueue_script('jquery');
+	}
+}
+
+add_action( 'init', 'register_jquery' );
 add_action( 'wp_enqueue_scripts', 'resources' );
