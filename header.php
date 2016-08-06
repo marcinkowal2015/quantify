a783ebdbc3e255403a406ebd7167fe8d9044c1de
marcinkowal2015 <marcinkowal2015@outlook.com>
2016-08-06 16:13:02 +0200

    Added first version of menu

--- header.php
+++ header.php
@@ -9,23 +9,54 @@
 <body <?php body_class(); ?>>
 
 <header>
-    <div class="header__bar">
-        <img src="<?php echo get_template_directory_uri()?>/img/logo.png">
-        <div class="header__navigation">
-            <ul class="header__main-menu">
-                <li class="header__main-menu__item">item</li>
-                <li class="header__main-menu__item">item</li>
-                <li class="header__main-menu__item">item</li>
-                <li class="header__main-menu__item">item</li>
-            </ul>
-            <div class="header__divider">
-                |
-            </div>
-            <div class="header__language">
-                <span class="header__language__pl">pl</span>
-                <span>en</span>
+    <div class="header">
+        <div class="header__bar">
+            <img src="<?php echo get_template_directory_uri()?>/img/logo.png">
+            <div class="header__navigation">
+                <ul class="header__main-menu">
+                    <li class="header__main-menu__item">item</li>
+                    <li class="header__main-menu__item">item</li>
+                    <li class="header__main-menu__item">item</li>
+                    <li class="header__main-menu__item">item</li>
+                </ul>
+                <div class="header__divider">
+                    |
+                </div>
+                <div class="header__language">
+                    <span class="header__language__pl">pl</span>
+                    <span>en</span>
+                </div>
             </div>
         </div>
     </div>
+    <div class="navigation">
+        <div class="navigation__bar">
+            <div class="navigation__bar__list">
+                <div class="navigation__bar__list__title">title</div>
+                <ul>
+                    <li>aaa</li>
+                    <li>aaa</li>
+                    <li>aaa</li>
+                </ul>
+            </div>
 
+            <div class="navigation__bar__list">
+                <div class="navigation__bar__list__title">title</div>
+                <ul>
+                    <li>aaa</li>
+                    <li>aaa</li>
+                    <li>aaa</li>
+                </ul>
+            </div>
+
+            <div class="navigation__bar__list">
+                <div class="navigation__bar__list__title">title</div>
+                <ul>
+                    <li>aaa</li>
+                    <li>aaa</li>
+                    <li>aaa</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </header>
